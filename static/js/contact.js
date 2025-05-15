cc3c245435b9a10fb21b0adfd59d5eafbf940c62
gpjunagadh <website@gpjun.gujgov.edu.in>
2025-05-15 22:33:11 +0530

    Add files via upload

---
+++ static/js/contact.js
@@ -0,0 +1,3 @@
+function openMap() {
+    window.open("https://www.google.co.in/maps/place/GOVERNMENT+POLYTECHNIC+JUNAGADH/@21.4386134,70.5006246,17z/data=!3m1!4b1!4m6!3m5!1s0x3958019813d1351d:0xc679571ef67f842c!8m2!3d21.4386134!4d70.5031995!16s%2Fg%2F12610w2f0?entry=ttu&g_ep=EgoyMDI1MDQwOS4wIKXMDSoASAFQAw%3D%3D", "_blank");
+}
